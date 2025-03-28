f2189dc5ea1700973090072d48520a1aaf1da452
Jayanthmurala <Jayanthmurala@gmail.com>
2025-03-28 20:07:07 +0530

    Final Update

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 // Use our routes under /api
 app.use("/api", routes);
 app.use("/", (req, res) => {
-  res.send.json(
+  res.send(
     "Welcome to the Mini Query Engine API! Use /api/login to get a token."
   );
 });
